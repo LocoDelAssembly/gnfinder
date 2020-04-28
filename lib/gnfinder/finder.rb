5eda14524ba923f301a5497f2ba059ea515813f4
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-04-28 19:34:12 -0300

    Added extra response formats

--- lib/gnfinder/finder.rb
+++ lib/gnfinder/finder.rb
@@ -23,7 +23,7 @@ module Gnfinder
     attach_function(:find_go, :FindNamesToJSON,
                     %i[string string parser_callback], :void)
 
-    def self.find_names(text, opts = {})
+    def self.find_names_json(text, opts = {})
       params = {}
       params[:no_bayes] = true if opts[:no_bayes]
       params[:language] = opts[:language] if opts[:language].to_s.strip != ''
@@ -43,39 +43,54 @@ module Gnfinder
       callback = FFI::Function.new(:void, [:string]) { |str| output = str }
       find_go(text, params.to_json, callback)
 
-      if output
+      output
+    end
+
+    def self.find_names_hash(text, opts = {})
+      find_names_converted(text, { klass: Hash, delete: :delete }, opts)
+    end
+
+    def self.find_names(text, opts = {})
+      find_names_converted(text, { klass: OpenStruct, delete: :delete_field }, opts)
+    end
+
+    private
+
+    def self.find_names_converted(text, container, opts = {})
+      if output = find_names_json(text, opts)
         output = JSON.parse(output)
         output["metadata"].each_pair { |k, v| output[k] = v }
         output.delete("metadata")
 
-        output = convert_snake_case(output)
-        if output.total_words
-          output.total_tokens = output.total_words
-          output.delete_field("total_words")
+        output = convert_snake_case(output, container[:klass])
+
+        if output["total_words"]
+          output["total_tokens"] = output["total_words"]
+          output.send(container[:delete], "total_words")
         end
 
-        output.names ||= []
-        output.names.each do |name| 
+        output["names"] ||= []
+        output["names"].each do |name|
           [%w[start offset_start], %w[end offset_end], %w[annotation_nomen annot_nomen], %w[annotation_nomen_type annot_nomen_type]].each do |pair|
             if name[pair[0]]
               name[pair[1]] = name[pair[0]]
-              name.delete_field(pair[0])
+              name.send(container[:delete], pair[0])
             end
           end
 
-          name.odds ||= 0.0
-          name.offset_start ||=  0
-          name.offset_end ||=  0
-          name.annot_nomen_type = name.annot_nomen_type.to_sym if name.annot_nomen_type
+          name["odds"] ||= 0.0
+          name["offset_start"] ||=  0
+          name["offset_end"] ||=  0
+          name["annot_nomen_type"] = name["annot_nomen_type"].to_sym if name["annot_nomen_type"]
 
           best_result = name.dig(:verification, :best_result)
 
           if best_result
             best_result.match_type = getMatchType(best_result.match_type)
 
-            if best_result.classification_i_ds
-              best_result["classification_ids"] = best_result.classification_i_ds
-              best_result.delete_field("classification_i_ds")
+            if best_result["classification_i_ds"]
+              best_result["classification_ids"] = best_result["classification_i_ds"]
+              best_result.send(container[:delete], "classification_i_ds")
             end
           end
         end
@@ -84,10 +99,8 @@ module Gnfinder
       output
     end
 
-    private
-
-    def self.convert_snake_case(hash)
-      res = OpenStruct.new
+    def self.convert_snake_case(hash, klass)
+      res = klass.new
 
       hash.each do |key, value|
         str = key.gsub(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
@@ -95,12 +108,12 @@ module Gnfinder
         str.downcase!
 
         if value.kind_of?(Hash)
-          value = convert_snake_case(value)
+          value = convert_snake_case(value, klass)
         elsif value.kind_of?(Array)
-          value = value.map { |v| v.kind_of?(Hash) ? convert_snake_case(v) : v  }
+          value = value.map { |v| v.kind_of?(Hash) ? convert_snake_case(v, klass) : v  }
         end
 
-        res[str.to_sym] =  value
+        res[str] =  value
       end
 
       res
